bc963b866751ed66df6424ecc4d6a05406972511
AyushCoding <65083462+AyushCoding@users.noreply.github.com>
2020-10-06 10:53:55 +0530

    Writing the efficient solution
    Here for maximum function the header file has been updated and efficient solution for Kadane algorithm has been written.

--- Kadane's .cpp
+++ Kadane's .cpp
@@ -1,19 +1,16 @@
 #include<iostream>
 #include<climits>
+#include<algorithm>
 using namespace std;
 
 int maxSubArraySum(int a[], int size)
 {
-    int max_so_far = INT_MIN, max_ending_here = 0;
+    int max_so_far = max_ending_here = a[0];
 
-    for (int i = 0; i < size; i++)
+    for (int i = 1; i < size; i++)
     {
-        max_ending_here = max_ending_here + a[i];
-        if (max_so_far < max_ending_here)
-            max_so_far = max_ending_here;
-
-        if (max_ending_here < 0)
-            max_ending_here = 0;
+        max_ending_here = max(a[i],a[i] + max_ending_here);
+        max_so_far=max(max_so_far , max_ending_here);
     }
     return max_so_far;
 }
